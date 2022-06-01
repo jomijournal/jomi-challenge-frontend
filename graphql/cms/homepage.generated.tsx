561f3c55332bd1ecec94cd8adf2a6f2a3b133cf5
Brando Flores <brandoflores16@gmail.com>
2022-05-31 23:32:30 -0700

    Filled in needed fields for query

--- graphql/cms/homepage.generated.tsx
+++ graphql/cms/homepage.generated.tsx
@@ -6,7 +6,7 @@ const defaultOptions = {} as const;
 export type HomePageQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
-export type HomePageQuery = { __typename?: 'Query', homePage?: { __typename?: 'HomePageEntityResponse', data?: { __typename?: 'HomePageEntity', attributes?: { __typename?: 'HomePage', sections?: Array<{ __typename?: 'ComponentCommonCarousel', id: string } | { __typename?: 'ComponentCommonHeader', id: string } | { __typename?: 'ComponentCommonTwoColumnBlock', id: string } | { __typename?: 'Error' } | null> | null } | null } | null } | null };
+export type HomePageQuery = { __typename?: 'Query', homePage?: { __typename?: 'HomePageEntityResponse', data?: { __typename?: 'HomePageEntity', attributes?: { __typename?: 'HomePage', sections?: Array<{ __typename?: 'ComponentCommonCarousel', id: string, Item?: Array<{ __typename?: 'ComponentCommonTwoColumnBlock', TitleText?: string | null } | null> | null } | { __typename?: 'ComponentCommonHeader', id: string, Text?: string | null, ButtonText?: string | null, ButtonLink?: string | null } | { __typename?: 'ComponentCommonTwoColumnBlock', id: string, TitleText?: string | null, Description?: string | null, ButtonText?: string | null, ButtonUrl?: string | null, ImagePosition?: Types.Enum_Componentcommontwocolumnblock_Imageposition | null, Image?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', id?: string | null, attributes?: { __typename?: 'UploadFile', alternativeText?: string | null, caption?: string | null, createdAt?: any | null, ext?: string | null, formats?: any | null, hash: string, height?: number | null, mime: string, name: string, previewUrl?: string | null, provider: string, provider_metadata?: any | null, size: number, updatedAt?: any | null, url: string, width?: number | null } | null } | null } | null } | { __typename?: 'Error' } | null> | null } | null } | null } | null };
 
 
 export const HomePageDocument = gql`
@@ -17,12 +17,46 @@ export const HomePageDocument = gql`
         sections {
           ... on ComponentCommonHeader {
             id
+            Text
+            ButtonText
+            ButtonLink
           }
           ... on ComponentCommonCarousel {
             id
+            Item {
+              TitleText
+            }
           }
           ... on ComponentCommonTwoColumnBlock {
             id
+            TitleText
+            Description
+            ButtonText
+            ButtonUrl
+            ImagePosition
+            Image {
+              data {
+                id
+                attributes {
+                  alternativeText
+                  caption
+                  createdAt
+                  ext
+                  formats
+                  hash
+                  height
+                  mime
+                  name
+                  previewUrl
+                  provider
+                  provider_metadata
+                  size
+                  updatedAt
+                  url
+                  width
+                }
+              }
+            }
           }
         }
       }
