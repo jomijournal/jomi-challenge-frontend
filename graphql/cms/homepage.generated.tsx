a9e253a8488878dcdfacfa50a20ed7cfaab6d79e
ryan <choi.link227@gmail.com>
2022-06-14 00:25:03 -0400

    run gql gen

--- graphql/cms/homepage.generated.tsx
+++ graphql/cms/homepage.generated.tsx
@@ -6,57 +6,56 @@ const defaultOptions = {} as const;
 export type HomePageQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
-export type HomePageQuery = { __typename?: 'Query', homePage?: { __typename?: 'HomePageEntityResponse', data?: { __typename?: 'HomePageEntity', attributes?: { __typename?: 'HomePage', sections?: Array<{ __typename?: 'ComponentCommonCarousel', id: string } | { __typename?: 'ComponentCommonHeader', id: string } | { __typename?: 'ComponentCommonTwoColumnBlock', id: string } | { __typename?: 'Error' } | null> | null } | null } | null } | null };
+export type HomePageQuery = { __typename?: 'Query', homePage?: { __typename?: 'HomePageEntityResponse', data?: { __typename?: 'HomePageEntity', attributes?: { __typename?: 'HomePage', sections?: Array<{ __typename?: 'ComponentCommonCarousel', id: string, Item?: Array<{ __typename?: 'ComponentCommonTwoColumnBlock', id: string, TitleText?: string | null, Description?: string | null, ButtonText?: string | null, ButtonUrl?: string | null, ImagePosition?: Types.Enum_Componentcommontwocolumnblock_Imageposition | null, Image?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', id?: string | null, attributes?: { __typename?: 'UploadFile', name: string, url: string } | null } | null } | null } | null> | null } | { __typename?: 'ComponentCommonHeader', id: string, Text?: string | null, ButtonText?: string | null, ButtonLink?: string | null } | { __typename?: 'ComponentCommonTwoColumnBlock', id: string, TitleText?: string | null, Description?: string | null, ButtonText?: string | null, ButtonUrl?: string | null, ImagePosition?: Types.Enum_Componentcommontwocolumnblock_Imageposition | null, Image?: { __typename?: 'UploadFileEntityResponse', data?: { __typename?: 'UploadFileEntity', id?: string | null, attributes?: { __typename?: 'UploadFile', name: string, url: string } | null } | null } | null } | { __typename?: 'Error' } | null> | null } | null } | null } | null };
 
 
 export const HomePageDocument = gql`
-  query HomePage {
-    homePage {
-      data {
-        attributes {
-          sections {
-            ... on ComponentCommonHeader {
-              id
-              Text
-              ButtonText
-              ButtonLink
-            }
-            ... on ComponentCommonCarousel {
-              id
-              Item {
-                ... on ComponentCommonTwoColumnBlock {
-                  id
-                  TitleText
-                  Description
-                  ButtonText
-                  ButtonUrl
-                  ImagePosition
-                  Image {
-                    data {
-                      id
-                      attributes {
-                        name
-                        url
-                      }
+    query HomePage {
+  homePage {
+    data {
+      attributes {
+        sections {
+          ... on ComponentCommonHeader {
+            id
+            Text
+            ButtonText
+            ButtonLink
+          }
+          ... on ComponentCommonCarousel {
+            id
+            Item {
+              ... on ComponentCommonTwoColumnBlock {
+                id
+                TitleText
+                Description
+                ButtonText
+                ButtonUrl
+                ImagePosition
+                Image {
+                  data {
+                    id
+                    attributes {
+                      name
+                      url
                     }
                   }
                 }
               }
             }
-            ... on ComponentCommonTwoColumnBlock {
-              id
-              TitleText
-              Description
-              ButtonText
-              ButtonUrl
-              ImagePosition
-              Image {
-                data {
-                  id
-                  attributes {
-                    name
-                    url
-                  }
+          }
+          ... on ComponentCommonTwoColumnBlock {
+            id
+            TitleText
+            Description
+            ButtonText
+            ButtonUrl
+            ImagePosition
+            Image {
+              data {
+                id
+                attributes {
+                  name
+                  url
                 }
               }
             }
@@ -65,6 +64,7 @@ export const HomePageDocument = gql`
       }
     }
   }
+}
     `;
 
 /**
